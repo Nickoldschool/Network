3fea7724b02bb1ae18844414a4941a50539bd2f3
nickoldschool <nickoldschooll@gmail.com>
2020-08-03 04:47:53 +0300

    Add components

--- Network/EndPoint/CurrencyEndPoint.swift
+++ Network/EndPoint/CurrencyEndPoint.swift
@@ -10,6 +10,7 @@ import Foundation
 
 
 enum NetworkEnvironment {
+    
     case baseEnvironment
 }
 
@@ -19,35 +20,77 @@ public enum CurrencyApi {
     case fromBaseToCurrent(firstRate: String,secondRate: String)
     case timePeriodRate(firstRate: String, secondRate: String, startDate: String, endDate: String)
     
-    
-    
 }
 
 extension CurrencyApi: EndPointType {
     
-    
-    var environmentBaseURL : String {
+    var environmentBaseURL: String {
         switch NetworkManager.environment {
         case .baseEnvironment: return "https://api.exchangeratesapi.io/"
         }
     }
     
     var baseURL: URL {
-        guard let url = URL(string: environmentBaseURL) else { fatalError("baseURL could not be configured.")}
-        return url
+        
+        //MARK: - Comment
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.exchangeratesapi.io"
+        components.path = path
+        components.queryItems = queryItems
+
+        return components.url!
+        //MARK: - Comment
+        
+        //MARK: - Uncomment
+//        guard let url = URL(string: environmentBaseURL) else { fatalError("baseURL could not be configured.")}
+//        return url
+        //MARK: - Uncomment
+        
     }
     
     var path: String {
         switch self {
-        case .base(let rate):
-            return "latest?base=\(rate)"
-        case .fromBaseToCurrent(let firstRate, let secondRate):
-            return "latest?base=\(firstRate)&symbols=\(secondRate)"
-        case .timePeriodRate(let firstRate, let secondRate, let startDate, let endDate):
-            return "history?base=\(firstRate)&symbols=\(secondRate)&start_at=\(startDate)&end_at=\(endDate)"
-          
+            
+        //MARK: - Comment
+        case .base( _):
+            return "latest"
+        case .fromBaseToCurrent( _, _):
+            return "latest"
+        case .timePeriodRate( _, _, _, _):
+            return "history"
+        //MARK: - Comment
+            
+        //MARK: - Uncomment
+//        case .base(let rate):
+//            return "latest?base=\(rate)"
+//        case .fromBaseToCurrent(let firstRate, let secondRate):
+//            return "latest?base=\(firstRate)&symbols=\(secondRate)"
+//        case .timePeriodRate(let firstRate, let secondRate, let startDate, let endDate):
+//            return "history?base=\(firstRate)&symbols=\(secondRate)&start_at=\(startDate)&end_at=\(endDate)"
+        //MARK: - Uncomment
+            
         }
     }
+    
+    //MARK: - Comment
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .base(let rate):
+            return [URLQueryItem(name: "base",     value: rate)]
+            
+        case .fromBaseToCurrent(let firstRate, let secondRate):
+            return [URLQueryItem(name: "base",     value: firstRate),
+                    URLQueryItem(name: "symbols",  value: secondRate)]
+            
+        case .timePeriodRate(let firstRate, let secondRate, let startDate, let endDate):
+            return [URLQueryItem(name: "base",     value: firstRate),
+                    URLQueryItem(name: "symbols",  value: secondRate),
+                    URLQueryItem(name: "start_at", value: startDate),
+                    URLQueryItem(name: "end_at",   value: endDate)]
+        }
+    }
+    //MARK: - Comment
     
     var httpMethod: HTTPMethod {
         return .get
@@ -61,15 +104,6 @@ extension CurrencyApi: EndPointType {
             return .requestParameters(bodyParameters: nil,
                                       bodyEncoding: .jsonEncoding,
                                       urlParameters: ["base":rate])
-        //MARK: - Check if correct works!
-            
-          //MARK: - Delete
-//        case .newMovies(let page):
-//            return .requestParameters(bodyParameters: nil,
-//                                      bodyEncoding: .urlEncoding,
-//                                      urlParameters: ["page":page,
-//                                                      "api_key":NetworkManager.CurrencyAPIKey])
-          //MARK: - Delete
             
         default:
             return .request
@@ -81,11 +115,3 @@ extension CurrencyApi: EndPointType {
     }
 }
 
-//let base = "EUR"
-//
-//var urlComponents = URLComponents()
-//
-//urlComponents.scheme = "https"
-//urlComponents.host = "api.exchangeratesapi.io"
-//urlComponents.path = "/latest"
-//urlComponents.queryItems = [URLQueryItem(name: "base", value: base)]
